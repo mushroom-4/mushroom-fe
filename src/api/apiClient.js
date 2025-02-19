80ee3f6aeb2b66370044fb984037884c802ed4ef
yeongbinim <yeim.dev@gmail.com>
2025-02-19 15:31:47 +0900

    :bug: fix: 인증 만료된 토큰 발견하면 로그인화면으로 리다이렉션

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -23,6 +23,8 @@ export const request = async (url, method = "GET", body = null, isAuth = true, i
 
     if (response.status === 401) {
       removeToken(); // 401 발생 시 자동 로그아웃
+      alert('인증이 만료되었습니다. 다시 로그인해주세요.');
+      document.location.href = '/login';
       return { success: false, message: "인증이 만료되었습니다. 다시 로그인해주세요." };
     }
 
